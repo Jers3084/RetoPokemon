5cbd91584896f0c868a3fdaa295d97f1cd400b8b
Jers3084 <jericrdez.c03@ucamp.utel.edu.mx>
2022-03-26 22:14:58 -0600

    Update
    Update

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React from "react";
+import Navbar from "./components/navbar/Navbar";
+import {Card} from "./components/card/Card";
+
+function App() {
+
+  const arregloPokemon = [];
+
+  const x = 1;
+
+  const obtenerImagenPokemon = async (x) => {
+    const url = `https://pokeapi.co/api/v2/pokemon/${x}`;
+    const response = await fetch(
+      url
+    );
+    const data = await response.json();
+
+    const pokemon = {
+      nombre: data.name,
+      imagen: data.sprites.front_default
+    }
+    arregloPokemon.push(pokemon);
+  };
+
+  for (let i = x; i <= 20; i++) {
+    obtenerImagenPokemon(i);
+  }
+
+  return (
+    <>
+      <Navbar />
+      <Card arregloPokemon={arregloPokemon} />
+
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/Card/Card.jsx
@@ -0,0 +1,24 @@
+import React, { useEffect } from 'react'
+
+export const Card = ({ arregloPokemon }) => {
+  useEffect(() => {
+    console.log(arregloPokemon)
+  }, [arregloPokemon])
+
+  return (
+    <>
+      <div className="container">
+        {arregloPokemon.map((x) => {
+          return (
+            <div className="card" style={{ width: '30rem' }}>
+              <img src={x.imagen} className="card-img-top" alt="..."></img>
+              <div className="card-body">
+                <h5 className="card-title">{x.nombre}</h5>
+              </div>
+            </div>
+          )
+        })}
+      </div>
+    </>
+  )
+}
